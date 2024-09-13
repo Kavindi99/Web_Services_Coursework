23ec46e30704287981da4384e2b35e944a065ed0
Kavindi Vithanage <kavindivithanage23@gmail.com>
2024-09-14 00:18:34 +0530

    Updated

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return '<h1>Web Services Coursework</h1>'
+    return '<h1>Web Services Coursework22</h1>'
 
 if __name__ == '__main__':
     #app.run(debug=True)
